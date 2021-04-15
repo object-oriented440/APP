125bc1bcbbe045b0aa3b3d782deb17e4901c8eb3
object-oriented440 <2958017271@qq.com>
2021-04-15 19:52:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/app/MyAppActivity.java
@@ -0,0 +1,30 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import CustomTimePicker.*;
+
+public class MyAppActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        Button DemoButton = (Button)findViewById(R.id.Demo_Button);
+        DemoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyAppActivity.this, TimePickerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
